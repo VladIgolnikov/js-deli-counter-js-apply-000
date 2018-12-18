2061177ab1b87f1d6e85b3081d062037e04b38ea
peaceful-trainer-9354 <vladigolnikov@gmail.com>
2018-12-18 04:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(array, person) {
   array.push(person);
-  return 'Welcome, ' + person + '. You are number ' + array.length + ' in line.';
+  return `Welcome, ${person}. You are number ${array.length} in line.`;
 }
 
 function nowServing(array) {
